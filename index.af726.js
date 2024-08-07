fa2e1da99a4cb14a55a02382df41d90cafdf63f1
Jayce(刘智城) <jaycemrl@gmail.com>
2024-08-07 11:58:04 +0800

    1

--- index.af726.js
+++ index.af726.js
@@ -1,4 +1,4 @@
-System.register(["./application.29b3d.js"], function (_export, _context) {
+System.register(["./application.dcc5d.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
